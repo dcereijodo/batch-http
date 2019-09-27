5a4cfd5ccefe6eed4a00b397628c390249023fda
dcereijodo <dcereijo@digitalorigin.com>
2019-09-27 23:45:15 +0200

    inform about progress by logging a JSON message with a running count and timestamp

--- src/main/scala/com/pagantis/singer/flows/BatchHttp.scala
+++ src/main/scala/com/pagantis/singer/flows/BatchHttp.scala
@@ -1,15 +1,19 @@
 package com.pagantis.singer.flows
 
-import akka.actor.ActorSystem
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+import akka.actor.{Actor, ActorSystem, Props}
 import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{JsonFraming, StreamConverters}
 import com.typesafe.config.ConfigFactory
 
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContextExecutor}
 import scala.util.Success
+import spray.json._
 
 object BatchHttp extends App {
 
@@ -34,11 +38,37 @@ object BatchHttp extends App {
   val parallelism = config.getInt("flow.parallelism")
   val frameLength = config.getInt("flow.frame_length")
 
-  val startTime = System.nanoTime()
-
   // This shutdown sequence was copied from another related issue: https://github.com/akka/akka-http/issues/907#issuecomment-345288919
   def shutdownSequence =
     Http().shutdownAllConnectionPools
+
+  // A sideways actor will maintain state
+  class RunningSum extends Actor with akka.actor.ActorLogging {
+    var total: Long = 0
+    val startTime: LocalDateTime = LocalDateTime.now()
+
+    private def logCount(count: Long): Unit = {
+      val currentTime = LocalDateTime.now()
+      val logRecord = JsObject(
+        Map(
+          "requests_processed" -> JsNumber(total),
+          "timestamp" -> JsString(currentTime.format(DateTimeFormatter.ISO_DATE_TIME)),
+        )
+      )
+      log.info(s"${logRecord.compactPrint}")
+    }
+
+    override def receive: Receive = {
+      case increment: Int =>
+        total = total + increment
+        if(total % 5000 == 0) logCount(total)
+
+    }
+
+    override def postStop(): Unit = logCount(total)
+
+  }
+  val counter = system.actorOf(Props[RunningSum], "counter")
 
 
   import Request._
@@ -61,13 +91,17 @@ object BatchHttp extends App {
       .log(clazz)
       .mapAsync(parallelism)(parseResponse(_))
       .log(clazz)
+      .map {
+        line: String =>
+          counter ! 1
+          line
+      }
       .runForeach(println(_))
 
   Await.ready(flowComputation, Duration.Inf)
-
-  standardLogger.info(s"Total execution time: ${(System.nanoTime - startTime)/1000000000} seconds")
-
   Await.ready(shutdownSequence, Duration.Inf)
+  Await.ready(system.terminate, Duration.Inf)
+
 
   flowComputation.value match {
     case Some(Success(_)) => sys.exit(0)
