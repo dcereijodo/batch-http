a1a81b44a50fa2c0f738626f500edc5b73be25c7
dcereijodo <dcereijo@digitalorigin.com>
2019-12-14 17:27:18 +0100

    add tests for request construction exceptions

--- src/test/scala/com/pagantis/singer/flows/test/TestRequest.scala
+++ src/test/scala/com/pagantis/singer/flows/test/TestRequest.scala
@@ -2,7 +2,7 @@ package com.pagantis.singer.flows.test
 
 import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.model.{HttpMethods, HttpRequest, Uri}
-import com.pagantis.singer.flows.Request
+import com.pagantis.singer.flows.{InvalidRequestException, Request}
 import org.scalatest.{FlatSpec, Inside, Matchers}
 import spray.json.{DefaultJsonProtocol, JsNumber, JsObject, JsString, JsValue}
 
@@ -39,50 +39,72 @@ class TestRequest extends FlatSpec with Matchers with DefaultJsonProtocol with I
   "Request" should "create GET request" in {
     val request = Request.fromLine(wrapRequest(get).compactPrint)
 
-    inside(request) {
-      case Request(method, _, _) => method shouldBe HttpMethods.GET
+    inside(request.toAkkaRequest) {
+      case HttpRequest(HttpMethods.GET, _, headers, _, _) =>
+        headers should contain (RawHeader("Some header key", "Some header value"))
+      case _ => fail
     }
 
     val context = JsString("some_id")
-    inside(Request.fromLine(wrapRequest(get, Some(context)).compactPrint)) {
+    inside(request) {
       case Request(method, _, Some(requestContext)) =>
         method shouldBe HttpMethods.GET
         requestContext shouldBe context
       case _ => fail
     }
-
-    inside(request.toAkkaRequest) {
-      case HttpRequest(_, _, headers, _, _) =>
-        headers should contain (RawHeader("Some header key", "Some header value"))
-      case _ => fail
-    }
   }
 
   "Request" should "create POST request" in {
-    inside(Request.fromLine(wrapRequest(post).compactPrint)) {
-      case Request(method, _, _) => method shouldBe HttpMethods.POST
-    }
-
     val context = JsObject("context" -> JsObject(Map("type" -> JsString("order"), "id" -> JsNumber(746))))
-    val request =
-      Request
-        .fromLine(wrapRequest(
-            post,
-            Some(context)
-          ).compactPrint)
-
-    inside(request) {
-      case Request(method, _, Some(requestContext)) =>
-        method shouldBe HttpMethods.POST
-        requestContext shouldBe context
-      case _ => fail
-    }
+    val request = Request.fromLine(wrapRequest(post, Some(context)).compactPrint)
 
     inside(request.toAkkaRequest) {
-      case HttpRequest(_, Uri(_, _, path, rawQueryString, _), _, _, _) =>
+      case HttpRequest(HttpMethods.POST, Uri(_, _, path, rawQueryString, _), _, _, _) =>
         rawQueryString shouldBe Some("var=var_value_1")
         path.toString shouldBe "/some_path"
       case _ => fail
     }
+
+    inside(request) {
+      case Request(_, _, Some(requestContext)) =>
+        requestContext shouldBe context
+      case _ => fail
+    }
+  }
+
+  "Request" should "fail invalid representations" in {
+    assertThrows[InvalidRequestException] { Request.fromLine("""{}""") }
+
+    (intercept[InvalidRequestException] {
+      Request.fromLine("""{"request": {"method": "INVALID_METHOD"}}""")
+    } getMessage) shouldBe "'method' must be a valid HTTP method"
+
+    (intercept[InvalidRequestException] {
+      Request.fromLine("""{"request": {"method": 200}}""")
+    } getMessage) shouldBe "'method' must be a JSON string"
+
+    (intercept[InvalidRequestException] {
+      Request.fromLine("""{"request": {"method": "GET", "query": {"param": {}}}}""").toAkkaRequest
+    } getMessage) shouldBe "invalid query parameter {}: it must be a string or a number"
+
+    (intercept[InvalidRequestException] {
+      Request.fromLine("""{"request": {"method": "GET", "query": "query_val"}}""").toAkkaRequest
+    } getMessage) shouldBe "'query' member must be key-value map"
+
+    (intercept[InvalidRequestException] {
+      Request.fromLine("""{"request": {"method": "GET", "headers": {"header_1": {}}}}""").toAkkaRequest
+    } getMessage) shouldBe "invalid header (header_1,{}): it must be a string or a number"
+
+    (intercept[InvalidRequestException] {
+      Request.fromLine("""{"request": {"method": "GET", "headers": "header_val"}}""").toAkkaRequest
+    } getMessage) shouldBe "'headers' member must be key-value map"
+
+    (intercept[InvalidRequestException] {
+      Request.fromLine("""{"request": {"method": "POST", "body": "body_val"}}""").toAkkaRequest
+    } getMessage) shouldBe "'body' must be a full JSON object"
+
+    (intercept[InvalidRequestException] {
+      Request.fromLine("""{"request": {"method": "POST", "body": {}, "path": {}}}""").toAkkaRequest
+    } getMessage) shouldBe "'path' must be an JSON string"
   }
 }
